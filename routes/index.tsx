56a5843a07219700fb8e84ef700f22bd5261c253
brandeju <brandeju@oregonstate.edu>
2024-08-25 17:25:58 -0400

    added navigation elements and sidebar to homepage

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,21 +4,29 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
+    <div class="parent">
+      <div class="sidebar">
+        <h1 class="sidebar-title">Browse Candles</h1>
+        <ul>
+          <li>Candles in Alphabetical Order</li>
+          <li>Candles by Season</li>
+          <li>Candles by Holiday</li>
+          <li>All Halloween Candles</li>
+        </ul>
+      </div>
+      <div class="">
+        <h1 class="">Welcome to Too Candle For Handle</h1>
+        <h2>All the candle you can handle <span class="andmore">AND more!</span></h2>
+        <p class="">A candle database for candle fanatics. </p>
+        <ul>
+          <li><a href="/add-candle">Add a Candle to Database</a></li>
+          <li><a href="/add-brand">Add a Brand to Database</a></li>
+          <li>View Candles</li>
+        </ul>
+        <div>
+          <h1>What is this?</h1>
+          <p>This is an online hub for candle collectors to showcase items in their collection and view the candles in others' collections.</p>
+        </div>
       </div>
     </div>
   );
